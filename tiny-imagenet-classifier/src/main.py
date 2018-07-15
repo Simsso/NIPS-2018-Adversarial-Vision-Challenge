5dfa2a98471cf3d64280a15394ffabd4c04814f5
Timo Denk <github@timodenk.com>
2018-07-15 16:31:54 +0200

    Copy data set loader

--- tiny-imagenet-classifier/src/main.py
+++ tiny-imagenet-classifier/src/main.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
+import data.tiny_imagenet as tiny_imagenet
 
 
 def main(args=None):
-    return None
+    tiny_imagenet.batch_q('train', {'num_epochs': 1, 'batch_size': 100})
 
 
 if __name__ == '__main__':
