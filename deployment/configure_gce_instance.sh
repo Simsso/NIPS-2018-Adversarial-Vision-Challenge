6534ab0d6714ac7d0baf8cf68fea3d416a58af58
Samed Güner <doktorgibsonyt@gmail.com>
2018-07-20 16:05:12 +0200

    Use ansible --key-file instead of ssh-agent

--- deployment/configure_gce_instance.sh
+++ deployment/configure_gce_instance.sh
@@ -5,9 +5,6 @@ apt-get update -y
 apt-add-repository ppa:ansible/ansible
 apt-get install -y ansible
 
-echo "Configure GCE Instance SSH Keys .."
-cp nips-cloudbuilder $HOME/.ssh/nips-cloudbuilder
-
 echo "Generate Ansible Host Inventory .."
 printf "[nips_training]\n $1\n" > gcp-hosts
 
@@ -15,4 +12,4 @@ echo "Disable Ansible Host Key Checking .."
 export ANSIBLE_HOST_KEY_CHECKING=false
 
 echo "Configure Google Compute Engine Instance at $1 .."
-ansible-playbook -i gcp-hosts gce_playbook.yml
+ansible-playbook -i gcp-hosts gce_playbook.yml --key-file nips-cloudbuilder
