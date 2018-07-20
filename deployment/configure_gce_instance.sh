68a7f1c6d303cd283d6be0f72f2f6a6a2a10c7fc
Samed Güner <doktorgibsonyt@gmail.com>
2018-07-20 13:43:58 +0200

    Update Playbooks

--- deployment/configure_gce_instance.sh
+++ deployment/configure_gce_instance.sh
@@ -9,4 +9,4 @@ echo "Generate Ansible Host Inventory .."
 printf "[nips_training]\n $1\n" > gcp-hosts
 
 echo "Configure Google Compute Engine Instance at $1 .."
-ansible-playbook -i gcp-hosts gce_playbook.yml 
+ansible-playbook -i gcp-hosts gce_playbook.yml
