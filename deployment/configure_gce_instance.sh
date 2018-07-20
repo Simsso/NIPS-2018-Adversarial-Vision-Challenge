58f90ac25620965ff1736dbb757e1707bfb77383
Samed Güner <doktorgibsonyt@gmail.com>
2018-07-20 13:36:56 +0200

    Generate Ansible Inventory

--- deployment/configure_gce_instance.sh
+++ deployment/configure_gce_instance.sh
@@ -5,5 +5,8 @@ apt-get update -y
 apt-add-repository ppa:ansible/ansible
 apt-get install -y ansible
 
+echo "Generate Ansible Host Inventory .."
+printf "[nips_training]\n $1\n" > gcp-hosts
+
 echo "Configure Google Compute Engine Instance at $1 .."
-ansible-playbook gce_playbook.yml  --extra-vars "host=$1"
+ansible-playbook -i gcp-hosts gce_playbook.yml 
