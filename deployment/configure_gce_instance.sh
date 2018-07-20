7246ff5e95f7ed9475acea061de65af4f623e885
Samed Güner <doktorgibsonyt@gmail.com>
2018-07-20 13:26:03 +0200

    Update Ansible installation (2)

--- deployment/configure_gce_instance.sh
+++ deployment/configure_gce_instance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Install Ansible .."
+apt-get update -y
 apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install ansible
+apt-get install -y ansible
 
 echo "Configure Google Compute Engine Instance at $1 .."
 ansible-playbook gce_playbook.yml  --extra-vars "host=$1"
