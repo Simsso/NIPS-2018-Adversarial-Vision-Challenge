Add auxiliary softmax output after incep4a and after incep5a, and setup is_training tensor, fix dropout

tiny-imagenet-classifier/src/model/tiny_inception.py
@@ -62,7 +62,7 @@ def inception_layer(value, name, wd, reduction_filter_counts, conv_filter_counts
     return post_act
 
 
-def graph(inputs, dropout_prob, wd):
+def graph(inputs, dropout_prob, is_training, wd):
     with tf.variable_scope('', reuse=tf.AUTO_REUSE):
         tf.summary.image('input_image', inputs)
 
@@ -88,26 +88,52 @@ def graph(inputs, dropout_prob, wd):
 
         # Some more inception goodness
         incep4a = inception_layer(maxpool1, 'incep4a', wd, reduction_filter_counts=[96, 16, 64],  conv_filter_counts=[192, 208, 48])
+        # -------------- auxiliary softmax output branch 1 ---------------
+        softmax_aux_1 = tf.layers.average_pooling2d(incep4a, pool_size=[5, 5], strides=3)
+        softmax_aux_1 = tf.layers.conv2d(softmax_aux_1, filters=256, kernel_size=[1, 1], strides=1, name="softmax_aux_1/conv-1x1")
+        softmax_aux_1 = tf.nn.relu(softmax_aux_1)
+        print("after softmax_aux_1-pool: ", softmax_aux_1.get_shape().as_list())
+        softmax_aux_1 = tf.layers.dense(softmax_aux_1, units=256, activation=tf.nn.relu, name="softmax_aux_1/dense-1-256")
+        softmax_aux_1 = tf.layers.dropout(softmax_aux_1, rate=0.7, training=is_training)
+        softmax_aux_1 = tf.layers.dense(softmax_aux_1, units=data.NUM_CLASSES, activation=tf.nn.relu, name="softmax_aux_1/dense-out")
+        softmax_aux_1 = tf.nn.softmax(softmax_aux_1, axis=1, name="softmax_aux_1/softmax")
+        # ----------------------------------------------------------------
         incep4b = inception_layer(incep4a,  'incep4b', wd, reduction_filter_counts=[112, 24, 64], conv_filter_counts=[160, 224, 64])
         incep4c = inception_layer(incep4b,  'incep4c', wd, reduction_filter_counts=[128, 24, 64], conv_filter_counts=[128, 256, 64])
         maxpool2 = tf.layers.max_pooling2d(incep4c, pool_size=[3, 3], strides=2)
         print("after incep4a / 4b / 4c / maxpool: ", maxpool2.get_shape().as_list())
 
         incep5a = inception_layer(maxpool2, 'incep5a', wd, reduction_filter_counts=[160, 32, 128], conv_filter_counts=[256, 320, 128])
+        # -------------- auxiliary softmax output branch 2 ---------------
+        softmax_aux_2 = tf.layers.average_pooling2d(incep5a, pool_size=[4, 4], strides=1)
+        softmax_aux_2 = tf.layers.conv2d(softmax_aux_2, filters=256, kernel_size=[1, 1], strides=1, name="softmax_aux_2/conv-1x1")
+        softmax_aux_2 = tf.nn.relu(softmax_aux_2)
+        print("after softmax_aux_2-pool: ", softmax_aux_2.get_shape().as_list())
+        softmax_aux_2 = tf.layers.dense(softmax_aux_2, units=256, activation=tf.nn.relu, name="softmax_aux_2/dense-1-256")
+        softmax_aux_2 = tf.layers.dropout(softmax_aux_2, rate=0.7, training=is_training)
+        softmax_aux_2 = tf.layers.dense(softmax_aux_2, units=data.NUM_CLASSES, activation=tf.nn.relu, name="softmax_aux_2/dense-out")
+        softmax_aux_2 = tf.nn.softmax(softmax_aux_2, axis=1, name="softmax_aux_2/softmax")
+        # ----------------------------------------------------------------
         incep5b = inception_layer(incep5a,  'incep5b', wd, reduction_filter_counts=[192, 48, 128], conv_filter_counts=[384, 384, 128])
         # output has 384 + 384 + 128 + 128 = 1024 channels
 
         # average pooling (reduce to spatial 1x1)
         avgpool = tf.layers.average_pooling2d(incep5b, pool_size=[4, 4], strides=1)
-        print("after incep5a / 5b / 5c / avgpool: ", avgpool.get_shape().as_list())
+        print("after incep5a / 5b / avgpool: ", avgpool.get_shape().as_list())
         
         # dropout
-        dropout = tf.layers.dropout(avgpool, rate=dropout_prob)
+        dropout = tf.layers.dropout(avgpool, rate=dropout_prob, training=is_training)
 
         # one dense layer
         flat = tf.reshape(dropout, [-1, np.product(dropout.shape[1:])])
         logits = add_wd(tf.layers.dense(flat, units=data.NUM_CLASSES), wd)
-        softmax = tf.nn.softmax(logits, axis=1, name='softmax')
+
+        softmax = tf.nn.softmax(logits, axis=1)
+
+        # weight output with auxiliary softmax outputs, but only at training time, not at inference time
+        softmax = tf.cond(pred=is_training,
+                          true_fn=lambda: (0.7 * softmax + 0.15 * (softmax_aux_1 + softmax_aux_2)),
+                          false_fn=lambda: softmax)
 
     return logits, softmax
 

tiny-imagenet-classifier/src/trainer/sgd.py
@@ -22,12 +22,13 @@ def train(model_def):
         images = tf.placeholder(tf.float32, shape=[None, data.IMG_DIM, data.IMG_DIM, data.IMG_CHANNELS], name='images')
         labels = tf.placeholder(tf.uint8, shape=[None], name='labels')
         drop_prob = tf.placeholder(tf.float32, name='dropout_probability')
+        is_training = tf.placeholder(tf.bool, shape=(), name="is_training")
 
         # data set
         train_batch = data.batch_q('train', TRAIN_BATCH_SIZE)
         valid_batch = data.batch_q('val', VALID_BATCH_SIZE)
 
-        logits, softmax = model_def.graph(tf.cast(images, tf.float32), drop_prob, WEIGHT_DECAY)
+        logits, softmax = model_def.graph(tf.cast(images, tf.float32), drop_prob, is_training, WEIGHT_DECAY)
         loss = model_def.loss(labels, logits)
         tf.summary.scalar('loss', loss)
         acc = model_def.accuracy(labels, softmax)
@@ -61,7 +62,8 @@ def train(model_def):
                                                         feed_dict={
                                                             images: valid_images,
                                                             labels: valid_labels,
-                                                            drop_prob: 0
+                                                            drop_prob: 0,
+                                                            is_training: False
                                                         })
                             valid_log_writer.add_summary(summary, global_step=epoch * STEPS_PER_EPOCH + step)
                             print(acc_val)
@@ -71,7 +73,8 @@ def train(model_def):
                                               feed_dict={
                                                   images: train_images,
                                                   labels: train_labels,
-                                                  drop_prob: DROPOUT
+                                                  drop_prob: DROPOUT,
+                                                  is_training: True
                                               })
                         if step % STEPS_PER_VALIDATION == 0:
                             train_log_writer.add_summary(summary, global_step=epoch * STEPS_PER_EPOCH + step)