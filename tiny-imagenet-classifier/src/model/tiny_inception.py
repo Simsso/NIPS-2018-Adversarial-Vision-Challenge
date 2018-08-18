Revisit inception architecture; use Adam optimizer, add saver-logic to trainer Architecture changes: - add batch norm in a few places - add some missing activations - fix auxiliary softmax branches

tiny-imagenet-classifier/src/model/tiny_inception.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 import data.tiny_imagenet as data
 
-NAME = 'tiny_inception_001-aux'
+NAME = 'tiny_inception_002'
 
 
 def get_params(x):
@@ -23,7 +23,7 @@ def add_wd(op, wd):
     return op
 
 
-def inception_layer(value, name, wd, reduction_filter_counts, conv_filter_counts):
+def inception_layer(value, name, wd, reduction_filter_counts, conv_filter_counts, batch_norm=False, is_training=None):
     """
     Creates an inception layer.
 
@@ -34,7 +34,9 @@ def inception_layer(value, name, wd, reduction_filter_counts, conv_filter_counts
         - reduction_filter_counts: array of the number of filters used in the dimensionality reduction 1x1 convolutions
                                    where the order is as follows: [#(3x3 reduce), #(5x5 reduce), #(after-max-pooling)]
         - conv_filter_counts: array of the number of filters used for the non-reduction (1x1, 3x3, 5x5) convolutions
-                              in the order of [#(1x1 direct), #(3x3), #(5x5)]
+                              in the order of [#(1x1 direct), #(3x3), #(5x5)
+        - batch_norm: whether or not to apply batch norm before the final relu activations
+        - is_training: boolean tensor (only needed if batch_norm==True)
     """
 
     assert (len(reduction_filter_counts) == 3)
@@ -45,10 +47,12 @@ def inception_layer(value, name, wd, reduction_filter_counts, conv_filter_counts
 
     # 1x1 -> 3x3
     convb = add_wd(tf.layers.conv2d(value, filters=reduction_filter_counts[0], kernel_size=[1, 1], name='%s/conv-1x1-reduce-3x3' % name), wd)
+    convb = tf.nn.relu(convb)
     convb = add_wd(tf.layers.conv2d(convb, filters=conv_filter_counts[1], kernel_size=[3, 3], padding='same', name='%s/conv-3x3' % name), wd)
 
     # 1x1 -> 5x5
     convc = add_wd(tf.layers.conv2d(value, filters=reduction_filter_counts[1], kernel_size=[1, 1], name='%s/conv-1x1-reduce-5x5' % name), wd)
+    convc = tf.nn.relu(convc)
     convc = add_wd(tf.layers.conv2d(convc, filters=conv_filter_counts[2], kernel_size=[5, 5], padding='same', name='%s/convc2' % name), wd)
 
     # 3x3 max pooling -> 1x1
@@ -57,29 +61,35 @@ def inception_layer(value, name, wd, reduction_filter_counts, conv_filter_counts
 
     # add activations
     pre_act = tf.concat([conva, convb, convc, convd], axis=3, name=name)
-    post_act = tf.nn.relu(pre_act)
+    if batch_norm:
+        pre_act = tf.layers.batch_normalization(pre_act, training=is_training)
 
-    return post_act
+    return tf.nn.relu(pre_act)
 
 
 def auxiliary_softmax_branch(inputs, name, is_training, avg_pool_size=[5, 5], avg_pool_strides=3):
     softmax_aux = tf.layers.average_pooling2d(inputs, pool_size=avg_pool_size, strides=avg_pool_strides)
     softmax_aux = tf.layers.conv2d(softmax_aux, filters=256, kernel_size=[1, 1], strides=1, name=("%s/conv-1x1" % name))
+    softmax_aux = tf.layers.batch_normalization(softmax_aux, training=is_training)
     softmax_aux = tf.nn.relu(softmax_aux)
     print(("after %s-pool: " % name), softmax_aux.get_shape().as_list())
+
     softmax_aux = tf.layers.flatten(softmax_aux)
     softmax_aux = tf.layers.dense(softmax_aux, units=256, activation=tf.nn.relu, name=("%s/dense-1-256" % name))
     softmax_aux = tf.layers.dropout(softmax_aux, rate=0.7, training=is_training)
-    softmax_aux = tf.layers.dense(softmax_aux, units=data.NUM_CLASSES, activation=tf.nn.relu, name=("%s/dense-out" % name))
-    softmax_aux = tf.nn.softmax(softmax_aux, axis=1, name=("%s/softmax" % name))
-    return softmax_aux
 
-def graph(inputs, dropout_prob, is_training, wd):
+    logits = tf.layers.dense(softmax_aux, units=data.NUM_CLASSES, name=("%s/logits-out" % name))
+    softmax = tf.nn.softmax(logits, axis=1, name=("%s/softmax" % name))
+
+    return logits, softmax
+
+def graph(inputs, is_training, dropout_prob, wd):
     with tf.variable_scope('', reuse=tf.AUTO_REUSE):
         tf.summary.image('input_image', inputs)
 
         # initial conv layer 1
         conv1 = add_wd(tf.layers.conv2d(inputs, filters=64, kernel_size=[5, 5], strides=2, name="conv-initial-1"), wd)
+        conv1 = tf.layers.batch_normalization(conv1, training=is_training)
         conv1 = tf.nn.relu(conv1)
         conv1 = tf.layers.max_pooling2d(conv1, pool_size=[2, 2], strides=1)
 
@@ -87,7 +97,10 @@ def graph(inputs, dropout_prob, is_training, wd):
 
         # initial conv layer 2 (with 1x1 reduction as in GoogLeNet)
         conv2 = add_wd(tf.layers.conv2d(conv1, filters=64, kernel_size=[1, 1], name="conv-1x1-reduce-initial-2"), wd)
+        conv2 = tf.nn.relu(conv2)
         conv2 = add_wd(tf.layers.conv2d(conv2, filters=192, kernel_size=[3, 3], name="conv-initial-2"), wd)
+        conv2 = tf.layers.batch_normalization(conv2, training=is_training)
+        conv2 = tf.nn.relu(conv2)
         conv2 = tf.layers.max_pooling2d(conv2, pool_size=[2, 2], strides=1)
 
         print("after conv2: ", conv2.get_shape().as_list())
@@ -101,19 +114,21 @@ def graph(inputs, dropout_prob, is_training, wd):
         # Some more inception goodness
         incep4a = inception_layer(maxpool1, 'incep4a', wd, reduction_filter_counts=[96, 16, 64],  conv_filter_counts=[192, 208, 48])
         # -------------- auxiliary softmax output branch 1 ---------------
-        softmax_aux_1 = auxiliary_softmax_branch(incep4a, name="softmax_aux_1", is_training=is_training)
+        logits_aux_1, _ = auxiliary_softmax_branch(incep4a, name="softmax_aux_1", is_training=is_training)
         # ----------------------------------------------------------------
         incep4b = inception_layer(incep4a,  'incep4b', wd, reduction_filter_counts=[112, 24, 64], conv_filter_counts=[160, 224, 64])
-        incep4c = inception_layer(incep4b,  'incep4c', wd, reduction_filter_counts=[128, 24, 64], conv_filter_counts=[128, 256, 64])
+        incep4c = inception_layer(incep4b,  'incep4c', wd, reduction_filter_counts=[128, 24, 64], conv_filter_counts=[128, 256, 64],
+                                  batch_norm=True, is_training=is_training)
         maxpool2 = tf.layers.max_pooling2d(incep4c, pool_size=[3, 3], strides=2)
         print("after incep4a / 4b / 4c / maxpool: ", maxpool2.get_shape().as_list())
 
         incep5a = inception_layer(maxpool2, 'incep5a', wd, reduction_filter_counts=[160, 32, 128], conv_filter_counts=[256, 320, 128])
         # -------------- auxiliary softmax output branch 2 ---------------
-        softmax_aux_2 = auxiliary_softmax_branch(incep5a, name="softmax_aux_2", is_training=is_training,
-                                                 avg_pool_size=[4, 4], avg_pool_strides=1)
-        # ----------------------------------------------------------------
-        incep5b = inception_layer(incep5a,  'incep5b', wd, reduction_filter_counts=[192, 48, 128], conv_filter_counts=[384, 384, 128])
+        logits_aux_2, _ = auxiliary_softmax_branch(incep5a, name="softmax_aux_2", is_training=is_training,
+                                                   avg_pool_size=[4, 4], avg_pool_strides=1)
+        # ----------------------------------------------------------------  
+        incep5b = inception_layer(incep5a,  'incep5b', wd, reduction_filter_counts=[192, 48, 128], conv_filter_counts=[384, 384, 128], 
+                                  batch_norm=True, is_training=is_training)
         # output has 384 + 384 + 128 + 128 = 1024 channels
 
         # average pooling (reduce to spatial 1x1)
@@ -126,21 +141,24 @@ def graph(inputs, dropout_prob, is_training, wd):
 
         # one dense layer
         logits = add_wd(tf.layers.dense(dropout, units=data.NUM_CLASSES), wd)
-        softmax = tf.nn.softmax(logits, axis=1)
+        softmax = tf.nn.softmax(logits, axis=1, name="softmax")
 
-        # weight output with auxiliary softmax outputs, but only at training time, not at inference time
-        softmax = tf.cond(pred=is_training,
-                          true_fn=lambda: (0.7 * softmax + 0.15 * (softmax_aux_1 + softmax_aux_2)),
-                          false_fn=lambda: softmax)
-        softmax = tf.identity(softmax, name="softmax")
-
-    return logits, softmax
+    return (logits, logits_aux_1, logits_aux_2), softmax
 
 
 def loss(labels, logits):
+    logits_main, logits_aux_1, logits_aux_2 = logits
     labels_one_hot = tf.one_hot(labels, depth=data.NUM_CLASSES)
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels_one_hot, logits=logits)
-    cross_entropy_loss = tf.reduce_mean(cross_entropy, name='cross_entropy_loss')
+
+    cross_entropy_main = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels_one_hot, logits=logits_main)
+    cross_entropy_aux_1 = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels_one_hot, logits=logits_aux_1)
+    cross_entropy_aux_2 = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels_one_hot, logits=logits_aux_2)
+
+    # add auxiliary branches-loss weighted by 0.3 to normal 'out' cross entropy
+    cross_entropy_loss = tf.reduce_mean(cross_entropy_main, name='cross_entropy_loss')
+    cross_entropy_loss = cross_entropy_loss + 0.3 * tf.reduce_mean(cross_entropy_aux_1, name='cross_entropy_loss_aux_1')
+    cross_entropy_loss = cross_entropy_loss + 0.3 * tf.reduce_mean(cross_entropy_aux_2, name='cross_entropy_loss_aux_2')
+
     tf.add_to_collection('losses', cross_entropy_loss)
     total_loss = tf.add_n(tf.get_collection('losses'), name='total_loss')  # includes weight decay loss terms
     return total_loss

tiny-imagenet-classifier/src/trainer/sgd.py
@@ -4,7 +4,7 @@ import os
 import tensorflow as tf
 import util.file_system
 
-LEARNING_RATE = .002
+LEARNING_RATE = .005
 NUM_EPOCHS = 1000
 TRAIN_BATCH_SIZE = 64
 VALIDATION_BATCH_SIZE = 64  # does not affect training results; adjustment based on GPU RAM
@@ -13,6 +13,8 @@ TF_LOGS = os.path.join('..', 'tf_logs')
 WEIGHT_DECAY = 1e-4
 DROPOUT = .5
 
+def saver_path(model_name):
+    return "checkpoints/" + model_name + "_model.ckpt"
 
 def train(model_def):
     def run_validation():
@@ -68,12 +70,18 @@ def train(model_def):
 
         init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
         summary_merged = tf.summary.merge_all()
+        
+        saver = tf.train.Saver()
 
     util.file_system.create_dir(TF_LOGS)
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True  # dynamic GPU memory allocation
     sess = tf.Session(graph=graph, config=config)
+
+    previous_val_acc = -1
+    number_of_epochs_decreased = 0
+
     with sess.as_default():
         train_log_writer = tf.summary.FileWriter(os.path.join(TF_LOGS, '%s_train' % model_def.NAME), sess.graph)
         valid_log_writer = tf.summary.FileWriter(os.path.join(TF_LOGS, '%s_valid' % model_def.NAME), sess.graph)
@@ -82,6 +90,7 @@ def train(model_def):
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
         sess.run(init)
+        # saver.restore(sess, saver_path(model_def.NAME))
 
         try:
             while not coord.should_stop():
@@ -90,6 +99,16 @@ def train(model_def):
                     print("Starting epoch #{}".format(epoch))
                     valid_acc, valid_loss = run_validation()
                     print("Validation data: accuracy {}, loss {}".format(valid_acc, valid_loss))
+
+                    if valid_acc > previous_val_acc:
+                        number_of_epochs_decreased = 0
+                        saver.save(sess, saver_path(model_def.NAME))
+                        print("Saved current weights to %s" % (saver_path(model_def.NAME)))
+                    elif number_of_epochs_decreased > 5:
+                        print("Too many epochs with decreasing validation accuracy. Stopping training.")
+                        break
+                    else:
+                        number_of_epochs_decreased += 1
 
                     valid_acc, valid_loss = run_training()
                     print("Training data (with batch norm): accuracy {}, loss {}".format(valid_acc, valid_loss))
@@ -104,5 +123,5 @@ def train(model_def):
 def get_optimization_op(loss):
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.control_dependencies(update_ops):
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=LEARNING_RATE)
+        optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
         return optimizer.minimize(loss)