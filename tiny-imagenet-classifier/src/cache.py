f494ea45909cdcd1cc4b439d2ff2f9509fce2963
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2018-08-20 16:28:46 +0200

    Optimize cache script

--- tiny-imagenet-classifier/src/cache.py
+++ tiny-imagenet-classifier/src/cache.py
@@ -6,10 +6,15 @@ if __name__ == '__main__':
         os.makedirs(cache.CACHE_DIR)
 
     for mode in ['train', 'val']:
-        limit = None
-        all_images, _ = cache.load_tiny_image_net(mode=mode, limit=limit)
+        limit = 1 # for testing
 
         path = cache.get_cache_path(mode)
-        print("Starting calculation for %d %s-images, caching at %s" % (len(all_images), mode, path))
-        activations = cache.read_cache_or_generate_activations(cache_path=path, all_images=all_images)
+        labels_only = cache.activations_are_cached(path)
+        all_images, _ = cache.load_tiny_image_net(mode=mode, labels_only=labels_only, limit=limit)
+
+        print("Starting calculation for %d %s-images..." % (len(all_images), mode))
+        activations = cache.read_cache_or_generate_activations(cache_path=path, all_images=all_images, batch_size=200)
         print("Done. Result shape: ", activations.shape)
+
+        print(activations[0])
+        break
