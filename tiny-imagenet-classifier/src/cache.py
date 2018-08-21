1a1e3eaa7cb2c19009b7b973f363f8c6e476f548
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2018-08-21 08:17:44 +0200

    Add random data augmentation to transfer value caching

--- tiny-imagenet-classifier/src/cache.py
+++ tiny-imagenet-classifier/src/cache.py
@@ -1,20 +1,21 @@
 import util.cache_inception_transfer_values as cache
 import os
 
+LIMIT = 30  # for testing
+BATCH_SIZE = 2
+
 if __name__ == '__main__':
     if not os.path.exists(cache.CACHE_DIR):
         os.makedirs(cache.CACHE_DIR)
 
-    for mode in ['val', 'train']:
-        limit = None
-
+    for mode in ['train', 'val']:
         path = cache.get_cache_path(mode)
         labels_only = cache.activations_are_cached(path)
-        all_images, _ = cache.load_tiny_image_net(mode=mode, labels_only=labels_only, limit=limit)
+        all_images, _ = cache.load_tiny_image_net(mode=mode, labels_only=labels_only, limit=LIMIT)
 
-        print("Starting calculation for %d %s-images..." % (len(all_images), mode))
-        activations = cache.read_cache_or_generate_activations(cache_path=path, all_images=all_images, batch_size=200)
-        print("Done. Result shape: ", activations.shape)
-
-        print(activations[0])
-        break
+        print("Starting calculation for %d %s-images in batches of %d..." % (len(all_images), mode, BATCH_SIZE))
+        activations = cache.read_cache_or_generate_activations(cache_path=path,
+                                                               all_images=all_images,
+                                                               mode=mode,
+                                                               batch_size=BATCH_SIZE)
+        print("Done. Result shape: ", activations.shape, "\n")
--- tiny-imagenet-classifier/src/util/cache_inception_transfer_values.py
+++ tiny-imagenet-classifier/src/util/cache_inception_transfer_values.py
@@ -5,10 +5,13 @@ import pickle
 import numpy as np
 import os
 import sys
+from datetime import datetime 
 
 slim = tf.contrib.slim
 
 ACTIVATION_DIM = 2048
+NUMBER_OF_AUGMENTATION_EPOCHS = 3
+CROP_DIM = 56
 
 ##################### Tiny ImageNet Loading Helper ######################
 import cv2
@@ -45,29 +48,52 @@ def inception_v3_features(images):
     return features
 
 
-def run_batch(batch):
-    scaled_batch = tf.image.resize_images(images=batch, size=[299, 299])
-    scaled_batch = tf.map_fn(lambda frame: tf.image.per_image_standardization(frame), scaled_batch)  #this line was *it*!
-    activations = inception_v3_features(scaled_batch)
-    return activations
+def augment_normalize(image, mode):
+    image = tf.image.per_image_standardization(image)
+
+    if mode is not 'train':
+        # no further modifications other than deterministic cropping
+        return tf.image.crop_to_bounding_box(image, 4, 4, CROP_DIM, CROP_DIM)  
+
+    image = tf.random_crop(image, np.array([CROP_DIM, CROP_DIM, data.IMG_CHANNELS]))
+
+    # adjust number of augmentations based on augmentation epoch count 
+    if NUMBER_OF_AUGMENTATION_EPOCHS > 1:
+        image = tf.image.random_flip_left_right(image)
+
+    if NUMBER_OF_AUGMENTATION_EPOCHS > 2:
+        image = tf.image.random_saturation(image, lower=0.6, upper=1.4)
+    
+    return image
 
 
-def inference_in_batches(all_images, batch_size):
+def inference_in_batches(all_images, batch_size, mode):
     """Returns a numpy array of the activations of shape len(all_images)x2048
     - all_images: 2d numpy array of shape Nx(data.IMG_DIM)x(data.IMG_DIM)x(data.IMG_CHANNELS)
     """
     graph = tf.Graph()
     with graph.as_default():
-        batch_placeholder = tf.placeholder(tf.float32, shape=[None, data.IMG_DIM, data.IMG_DIM, data.IMG_CHANNELS])
-        activations = run_batch(batch=batch_placeholder)
+        images = tf.placeholder(tf.float32, shape=[None, data.IMG_DIM, data.IMG_DIM, data.IMG_CHANNELS])
+
+        # random augmentation & non-random normalization
+        augmented_images = tf.map_fn(lambda img: augment_normalize(img, mode), images)
+
+        # rescale to Inception-expected size
+        rescaled_batch = tf.image.resize_images(augmented_images, size=[299, 299])
+        activations = inception_v3_features(rescaled_batch)
 
         init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
         saver = inception_v3.create_saver()
 
     sess = tf.Session(graph=graph)
-    num_images = len(all_images)
+
+    # simply repeat all images so we have multiple augmentation epochs (for train mode only)
+    augmentation_epochs = NUMBER_OF_AUGMENTATION_EPOCHS if mode is 'train' else 1
+
+    num_images = len(all_images) * augmentation_epochs
+    all_images = np.repeat(all_images, repeats=augmentation_epochs, axis=0)
+
     result = np.ndarray(shape=(num_images, ACTIVATION_DIM))
-    
     with sess.as_default():
         sess.run(init)
         inception_v3.restore(sess, saver)
@@ -75,17 +101,31 @@ def inference_in_batches(all_images, batch_size):
         full_batches = num_images // batch_size
         num_batches = full_batches if num_images % batch_size == 0 else full_batches + 1
 
+        start_time = 0
+        end_time = 0
         for i in range(num_batches):
             msg = "\r- Processing batch: {0:>6} / {1}".format(i+1, num_batches)
+
+            # -- time tracking stats --
+            if start_time:
+                time_for_last_batch = end_time - start_time
+                estimated_remaining = (num_batches - i) * time_for_last_batch
+                msg += " (ETA: {})".format(estimated_remaining)
+            # -------------------------
             sys.stdout.write(msg)
             sys.stdout.flush()
+            # -------------------------
+
+            start_time = datetime.now()
 
             from_idx = i*batch_size
             to_idx = min((i+1)*batch_size, num_images)
-            batch_values = all_images[from_idx:to_idx]
+            images_batch = all_images[from_idx:to_idx]
             batch_result = sess.run(activations, feed_dict={
-                batch_placeholder: batch_values
+                images: images_batch
             })
+
+            end_time = datetime.now()
 
             result[from_idx:to_idx] = np.squeeze(batch_result)  # remove 1x dimensions
         print("") # new line
@@ -96,7 +136,7 @@ def inference_in_batches(all_images, batch_size):
 def activations_are_cached(cache_path):
     return os.path.exists(cache_path)
 
-def read_cache_or_generate_activations(cache_path, all_images, batch_size=64):
+def read_cache_or_generate_activations(cache_path, all_images, mode, batch_size=64):
     # If the cache-file exists
     if activations_are_cached(cache_path):
         # Load the cached data from the file
@@ -106,7 +146,7 @@ def read_cache_or_generate_activations(cache_path, all_images, batch_size=64):
     else:
         # The cache-file does not exist.
         # Generate activations
-        activations = inference_in_batches(all_images, batch_size=batch_size)
+        activations = inference_in_batches(all_images, batch_size=batch_size, mode=mode)
 
         # Save the data to a cache-file.
         with open(cache_path, mode='wb') as file:
