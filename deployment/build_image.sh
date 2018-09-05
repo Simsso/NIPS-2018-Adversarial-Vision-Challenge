1b792970066b0189283f7df40c109b0a02dbbe69
Samed Güner <doktorgibsonyt@gmail.com>
2018-09-05 10:02:42 +0200

    Add Versioning option using tags
    - Tags allow to version the given model. Please use it as <model folder name>-<[0-9.]*>

---
+++ deployment/build_image.sh
@@ -0,0 +1,49 @@
+
+echo "Retrieving model version and model folder name .."
+
+MODEL_VERSION=$( echo $2 | grep -o '[0-9.]*$')
+MODEL_FOLDER=${2%-$MODEL_VERSION}
+
+echo "Model Folder: $MODEL_FOLDER"
+echo "Model Version: $MODEL_VERSION"
+
+echo "Update repositories .."
+apt-get update 
+
+echo "Update required libs .."
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common curl
+
+echo "Add GPG keys .."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+echo "Add official Docker repository .."
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+echo "Update repositories .."
+apt-get update 
+
+echo "Install docker .."
+apt-get install -y docker-ce
+
+echo "Building Docker Image .."
+docker build -t gcr.io/$1/$MODEL_FOLDER:$MODEL_VERSION models/$MODEL_FOLDER
+
+echo "Pushing Docker Image .."
+docker push gcr.io/$1/$MODEL_FOLDER:$MODEL_VERSION
+
+# {
+#            "name":"gcr.io/cloud-builders/docker",
+ #           "args": [
+  #              "build",
+   #             "-t",
+    #            "gcr.io/$PROJECT_ID/$TAG_NAME:$SHORT_SHA",
+     #           "models/$TAG_NAME"
+      #      ]
+       # },
+      #  {
+       #     "name":"gcr.io/cloud-builders/docker",
+        #    "args": [
+         #       "push",
+          #      "gcr.io/$PROJECT_ID/$TAG_NAME:$SHORT_SHA"
+          #  ]
+       # },
