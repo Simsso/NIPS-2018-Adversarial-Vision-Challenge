640f1d6fed309729c9d918421c11690353eb8c49
Samed Güner <doktorgibsonyt@gmail.com>
2018-09-11 09:53:55 +0200

    Optimize Docker instructions

--- models/tiny-imagenet-classifier-model/start.sh
+++ models/tiny-imagenet-classifier-model/start.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Mounting of $1"
 # mount bucket
 gcsfuse $1 $1/
 
 # start training
-nohup tensorboard --logdir=$1/tf_logs > /dev/null &
+echo "Start training of $2"
+nohup tensorboard --logdir=$1/$2/tf_logs > /dev/null &
 python src/main.py
