27d9d692055f8009633ca71283fe135ee6856575
Samed Güner <doktorgibsonyt@gmail.com>
2018-09-11 11:26:24 +0200

    Fix gcsfuse wrong mount path

--- models/tiny-imagenet-classifier-model/start.sh
+++ models/tiny-imagenet-classifier-model/start.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Mounting of $BUCKET_NAME"
 # mount bucket
-gcsfuse $BUCKET_NAME $MODEL_ID/
+gcsfuse $BUCKET_NAME $BUCKET_NAME/
 
 # create output folder
 mkdir -p $BUCKET_NAME/$MODEL_ID
 
 # start training
-echo "Start training of $LOG_FOLDER"
 nohup tensorboard --logdir=$BUCKET_NAME/$MODEL_ID/tf_logs > /dev/null &
 python src/main.py
