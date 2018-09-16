621ba0e8c040bd3e0cfa9289728432d22cad9576
Timo Denk <github@timodenk.com>
2018-09-16 09:16:09 +0200

    Remove sub-tests

--- experiments/vq-layer/layer-function/vq_layer/test/test_input_validation.py
+++ experiments/vq-layer/layer-function/vq_layer/test/test_input_validation.py
@@ -13,52 +13,44 @@ class TestInputValidation(TFTestCase):
 
     def test_x_shape(self):
         """Input x must have be 3-dimensional."""
-        i = 0
         # invalid inputs
-        for i, x in enumerate([
+        for x in [
             tf.placeholder(tf.float32, [None, 5]),
             tf.placeholder(tf.float32, [None, 5, 4, 8]),
             tf.placeholder(tf.float32, [1, 5, 4, 8]),
             tf.placeholder(tf.float32, ())
-        ]):
-            with self.subTest(i=i):
-                with self.assertRaises(ValueError):
-                    vector_quantization(x, 2)
+        ]:
+            with self.assertRaises(ValueError):
+                vector_quantization(x, 2)
 
         # valid input
-        with self.subTest(i=i+1):
-            vector_quantization(tf.placeholder(tf.float32, [None, 1, 5]), 5)
+        vector_quantization(tf.placeholder(tf.float32, [None, 1, 5]), 5)
 
     def test_n_is_positive(self):
         """Input n (number of vectors in the embedding space) must be greater than 0."""
-        for i, n in enumerate([-10000, -5, -1, 0]):
-            with self.subTest(i=i):
-                with self.assertRaises(ValueError):
-                    vector_quantization(tf.placeholder(tf.float32, [None, 1, 3]), n)
+        for n in [-10000, -5, -1, 0]:
+            with self.assertRaises(ValueError):
+                vector_quantization(tf.placeholder(tf.float32, [None, 1, 3]), n)
 
     def test_lookup_ord_is_valid(self):
         """Input lookup_ord must be an input that is valid for tf.norm's order parameter."""
         # valid inputs
         n = 5
-        i = 0
-        for i, lookup_ord in enumerate([1, 2, np.inf]):
-            with self.subTest(i=i):
-                self.setUp()
-                vector_quantization(tf.placeholder(tf.float32, [None, 1, 3]), n, lookup_ord=lookup_ord)
+        for lookup_ord in [1, 2, np.inf]:
+            self.setUp()
+            vector_quantization(tf.placeholder(tf.float32, [None, 1, 3]), n, lookup_ord=lookup_ord)
 
         # invalid inputs
-        for j, lookup_ord in enumerate(['fro', 'euclidean', -1, 0, 5]):
-            with self.subTest(i=i+1+j):
-                with self.assertRaises(ValueError):
-                    vector_quantization(tf.placeholder(tf.float32, [None, 1, 3]), n, lookup_ord=lookup_ord)
+        for lookup_ord in ['fro', 'euclidean', -1, 0, 5]:
+            with self.assertRaises(ValueError):
+                vector_quantization(tf.placeholder(tf.float32, [None, 1, 3]), n, lookup_ord=lookup_ord)
 
     def test_num_splits_gt_zero(self):
         """Input num_splits must be greater than 0."""
         # invalid inputs
-        for i, num_splits in enumerate([-5, -1, 0]):
-            with self.subTest(i=i):
-                with self.assertRaises(ValueError):
-                    vector_quantization(tf.placeholder(tf.float32, [None, 1, 3]), 4, num_splits=num_splits)
+        for num_splits in [-5, -1, 0]:
+            with self.assertRaises(ValueError):
+                vector_quantization(tf.placeholder(tf.float32, [None, 1, 3]), 4, num_splits=num_splits)
 
         # valid input (1 is always valid)
         vector_quantization(tf.placeholder(tf.float32, [None, 1, 3]), 4, num_splits=1)
