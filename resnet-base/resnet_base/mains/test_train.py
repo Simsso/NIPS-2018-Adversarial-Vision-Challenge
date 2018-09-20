c0078dcd83c209ccccd1c12e412ed802aedebab9
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2018-09-20 07:40:15 +0200

    Adapt trainer to new data pipeline

--- resnet-base/resnet_base/mains/test_train.py
+++ resnet-base/resnet_base/mains/test_train.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from resnet_base.model.resnet import ResNet
 from resnet_base.trainer.resnet_trainer import ResNetTrainer
+from resnet_base.data.tiny_imagenet_pipeline import TinyImageNetPipeline
 
 
 def main(args):
@@ -8,8 +9,11 @@ def main(args):
     config.gpu_options.allow_growth = True  # dynamic GPU memory allocation
     sess = tf.Session(config=config)
 
-    model = ResNet()
-    trainer = ResNetTrainer(sess, model)
+    pipeline = TinyImageNetPipeline()
+    imgs, labels = pipeline.get_iterator().get_next()
+    model = ResNet(imgs, labels)
+
+    trainer = ResNetTrainer(sess, model, pipeline)
     trainer.train()
 
 
--- resnet-base/resnet_base/trainer/resnet_trainer.py
+++ resnet-base/resnet_base/trainer/resnet_trainer.py
@@ -3,15 +3,16 @@ import numpy as np
 
 from resnet_base.trainer.base_trainer import BaseTrainer
 from resnet_base.model.resnet import ResNet
-import resnet_base.data.tiny_imagenet as data   # temporarily
+from resnet_base.data.tiny_imagenet_pipeline import TinyImageNetPipeline
 
 FLAGS = tf.flags.FLAGS
 
 
 class ResNetTrainer(BaseTrainer):
-    def __init__(self, sess: tf.Session, model: ResNet):
+    def __init__(self, sess: tf.Session, model: ResNet, pipeline: TinyImageNetPipeline):
         super().__init__(sess, model)
         self.resnet = model
+        self.pipeline = pipeline
         self.__build_train_op()
 
     def __build_train_op(self):
@@ -25,70 +26,59 @@ class ResNetTrainer(BaseTrainer):
             self.train_op = optimizer.minimize(self.resnet.loss)
 
     def train_epoch(self):
-        # TODO incorporate updated tf.data input pipeline when done, add summaries (abstracted in Logger class)
         batch_size = FLAGS.train_batch_size
-        num_samples = data.NUM_TRAIN_SAMPLES
-        batch_queue = data.batch_q('train', batch_size)
+        num_samples = TinyImageNetPipeline.num_train_samples
 
-        accuracy_mean, loss_mean = ResNetTrainer.__generic_epoch_with_params(batch_size, num_samples, batch_queue,
+        self.pipeline.switch_to(tf.estimator.ModeKeys.TRAIN)
+        accuracy_mean, loss_mean = ResNetTrainer.__generic_epoch_with_params(batch_size, num_samples,
                                                                              batch_step=self.train_step)
         tf.logging.info("Training metrics: accuracy = {}, loss = {}".format(accuracy_mean, loss_mean))
         self.sess.run(self.model.increment_current_epoch)
 
-    def train_step(self, batch_queue) -> (float, float):
+    def train_step(self) -> (float, float):
         """
         Performs one training step (i.e. one batch) - and returns the batch's accuracy and loss.
-        :param batch_queue: the queue used to acquire the next batch (TODO other tf.data pipeline)
         :return: a tuple of (accuracy, loss) for this training step
         """
-        images, labels = self.sess.run(batch_queue)
         _, accuracy, loss = self.sess.run([self.train_op, self.resnet.accuracy, self.resnet.loss],
                                           feed_dict={
-                                              self.resnet.x: images,
-                                              self.resnet.labels: labels,
                                               self.resnet.is_training: True
                                           })
         return accuracy, loss
 
     def val_epoch(self):
         batch_size = FLAGS.val_batch_size
-        num_samples = data.NUM_VALIDATION_SAMPLES
-        batch_queue = data.batch_q('val', batch_size)
+        num_samples = TinyImageNetPipeline.num_valid_samples
 
-        accuracy_mean, loss_mean = ResNetTrainer.__generic_epoch_with_params(batch_size, num_samples, batch_queue,
+        self.pipeline.switch_to(tf.estimator.ModeKeys.EVAL)
+        accuracy_mean, loss_mean = ResNetTrainer.__generic_epoch_with_params(batch_size, num_samples,
                                                                              batch_step=self.val_step)
         tf.logging.info("Validation metrics: accuracy = {}, loss = {}".format(accuracy_mean, loss_mean))
 
-    def val_step(self, batch_queue) -> (float, float):
+    def val_step(self) -> (float, float):
         """
         Performs one validation step (i.e. one batch) - and returns the batch's accuracy and loss.
-        :param batch_queue: the queue used to acquire the next batch (TODO other tf.data pipeline)
         :return: a tuple of (accuracy, loss) for this validation step
         """
-        images, labels = self.sess.run(batch_queue)
         accuracy, loss = self.sess.run([self.resnet.accuracy, self.resnet.loss],
                                        feed_dict={
-                                           self.resnet.x: images,
-                                           self.resnet.labels: labels,
                                            self.resnet.is_training: False
                                        })
         return accuracy, loss
 
     @staticmethod
-    def __generic_epoch_with_params(batch_size: int, num_samples: int, batch_queue, batch_step)\
-            -> (float, float):
+    def __generic_epoch_with_params(batch_size: int, num_samples: int, batch_step) -> (float, float):
         """
         Runs one epoch with the given parameters. Calls the given step function for each batch.
         :param batch_size: the number of samples used at every step
         :param num_samples: the total size of the data set
-        :param batch_queue: the queue used to acquire the next batch (TODO other tf.data pipeline)
-        :param batch_step: a function that takes a batch_queue and returns accuracy and loss values for the batch
+        :param batch_step: a function that runs and returns the accuracy and loss values for the batch
         :return: a tuple of (accuracy, loss), averaged over all batches
         """
         metrics = []
         num_batches = num_samples // batch_size
         for i in range(num_batches):
-            accuracy, loss = batch_step(batch_queue)
+            accuracy, loss = batch_step()
             metrics.append((accuracy, loss))
 
         accuracy_mean, loss_mean = np.mean(metrics, axis=0)
