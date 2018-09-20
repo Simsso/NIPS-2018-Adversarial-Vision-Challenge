e401a5af4f279022670d178ac3a29a57ecb5e6ea
Samed Güner <doktorgibsonyt@gmail.com>
2018-09-20 20:42:42 +0200

    Improve Tensorboard-Restarts

--- deployment/nips-tensorboard-vm-image/start.sh
+++ deployment/nips-tensorboard-vm-image/start.sh
@@ -3,7 +3,8 @@
 # start tensorboard
 while true
 do
-    nohup tensorboard --logdir=gs://nips-2018-adversarial-vision-challenge-data/model_data/ > /dev/null & sleep infinity
-    sleep 300
-    pkill -f tensorboard
+nohup tensorboard --logdir=gs://nips-2018-adversarial-vision-challenge-data/model_data/ &> /dev/null &
+sleep 300;
+pkill -f "tensorboard --logdir"
+echo "Restarting Tensorboard .."
 done
