effad1d746da26a8df08804dd75127012848418d
Samed Güner <doktorgibsonyt@gmail.com>
2018-09-20 19:47:03 +0200

    Add automatic Tensorboard restarts
    - due instability restart Tensorboard every 5 mins

--- deployment/nips-tensorboard-vm-image/start.sh
+++ deployment/nips-tensorboard-vm-image/start.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 # start tensorboard
-nohup tensorboard --logdir=gs://nips-2018-adversarial-vision-challenge-data/model_data/ > /dev/null & sleep infinity
+while true
+do
+    nohup tensorboard --logdir=gs://nips-2018-adversarial-vision-challenge-data/model_data/ > /dev/null & sleep infinity
+    sleep 300
+    pkill -f tensorboard
+done
