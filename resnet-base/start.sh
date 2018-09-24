ba3cf34b3ae39d2eec338894b32a8bbd32877387
Timo Denk <github@timodenk.com>
2018-09-24 20:05:58 +0200

    Add directory flags to start.sh

--- resnet-base/start.sh
+++ resnet-base/start.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 python setup.py install
-python -m resnet_base
+python -m resnet_base \
+    --pretrained_checkpoint "${HOME}/.data/tiny_imagenet_alp05_2018_06_26.ckpt" \
+    --data_dir "${HOME}/.data/tiny-imagenet-200" \
+    --train_log_dir "gs://${BUCKET_NAME}/model_data/${MODEL_ID}/train" \
+    --val_log_dir "gs://${BUCKET_NAME}/model_data/${MODEL_ID}/val"
+
+sleep infinity
