92e5e5d202309777760f9916d93ee57797f61b45
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2018-09-24 19:29:15 +0200

    Add tensorboard summaries to trainer files

--- resnet-base/resnet_base/trainer/base_trainer.py
+++ resnet-base/resnet_base/trainer/base_trainer.py
@@ -3,6 +3,8 @@ from resnet_base.model.base_model import BaseModel
 
 tf.flags.DEFINE_float("learning_rate", 1e-4, "The learning rate used for training.")
 tf.flags.DEFINE_integer("num_epochs", 10, "The number of epochs for which training is performed.")
+tf.flags.DEFINE_string("train_log_dir", "../tf_logs/train", "The directory used to save the training summaries.")
+tf.flags.DEFINE_string("val_log_dir", "../tf_logs/val", "The directory used to save the validation summaries.")
 
 FLAGS = tf.flags.FLAGS
 
@@ -11,26 +13,35 @@ class BaseTrainer:
     def __init__(self, model: BaseModel):
         self.model = model
         self.sess = tf.get_default_session()
+        self.__build_log_writer()
+
+    def __build_log_writer(self) -> None:
+        self.train_writer = tf.summary.FileWriter(FLAGS.train_log_dir, self.sess.graph)
+        self.val_writer = tf.summary.FileWriter(FLAGS.val_log_dir, self.sess.graph)
 
     def train(self) -> None:
         """
         Performs training for FLAGS.num_epochs epochs and runs one validation epoch after each training epoch.
         """
         init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+        summary_merged = tf.summary.merge_all()
         self.sess.run(init)
 
         # restore weights (as specified in the FLAGS)
-        self.model.load(self.sess)
+        self.model.restore(self.sess)
 
         # get the current epoch so we can re-start training from there
         start_epoch = self.model.current_epoch.eval(self.sess)
 
         for _ in range(start_epoch, FLAGS.num_epochs + 1):
             self.train_epoch()
-            self.sess.run(self.model.increment_current_epoch)
+            summary, _ = self.sess.run([summary_merged, self.model.increment_current_epoch])
+            self.train_writer.add_summary(summary, global_step=self.model.global_step)
 
             # run validation epoch to monitor training
             self.val_epoch()
+            summary = self.sess.run(summary_merged)
+            self.val_writer.add_summary(summary, global_step=self.model.global_step)
 
     def train_epoch(self) -> None:
         """
--- resnet-base/resnet_base/trainer/resnet_trainer.py
+++ resnet-base/resnet_base/trainer/resnet_trainer.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from collections import namedtuple
+from typing import List
 
 from resnet_base.trainer.base_trainer import BaseTrainer
 from resnet_base.model.resnet import ResNet
@@ -24,15 +25,30 @@ class ResNetTrainer(BaseTrainer):
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(update_ops):
             optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
-            self.train_op = optimizer.minimize(self.resnet.loss)
+            var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.resnet.custom_scope.name)
+            self.train_op = optimizer.minimize(self.resnet.loss, var_list=var_list)
 
     def train_epoch(self) -> None:
         num_samples = TinyImageNetPipeline.num_train_samples
 
         self.pipeline.switch_to(tf.estimator.ModeKeys.TRAIN)
         metrics = ResNetTrainer.__generic_epoch_with_params(self.pipeline.batch_size, num_samples,
-                                                            batch_step=self.train_step)
+                                                            batch_step=self.train_step, metrics_log=self.train_log)
         tf.logging.info("Training metrics: accuracy = {}, loss = {}".format(metrics.accuracy, metrics.loss))
+
+    def train_log(self, batch_index: int, batch_metrics: List[Metrics]):
+        log_every = 1
+        if not batch_index % log_every:
+            last_metrics = batch_metrics[-log_every:]
+            average_accuracy, average_loss = np.mean(np.array(last_metrics), axis=0)
+            num_batches_per_epoch = TinyImageNetPipeline.num_train_samples // self.pipeline.batch_size
+            progress_in_epoch = float(batch_index) / float(num_batches_per_epoch)
+
+            summary = tf.Summary(value=[
+                tf.Summary.Value(tag='accuracy', simple_value=average_accuracy),
+                tf.Summary.Value(tag='loss', simple_value=average_loss),
+            ])
+            self.train_writer.add_summary(summary, global_step=self.model.global_step.eval() + progress_in_epoch)
 
     def train_step(self) -> Metrics:
         """
@@ -43,6 +59,7 @@ class ResNetTrainer(BaseTrainer):
                                           feed_dict={
                                               self.resnet.is_training: True
                                           })
+        tf.logging.info("Train step metrics: accuracy = {}, loss = {}".format(accuracy, loss))
         return Metrics(accuracy, loss)
 
     def val_epoch(self) -> None:
@@ -65,12 +82,13 @@ class ResNetTrainer(BaseTrainer):
         return Metrics(accuracy, loss)
 
     @staticmethod
-    def __generic_epoch_with_params(batch_size: int, num_samples: int, batch_step) -> Metrics:
+    def __generic_epoch_with_params(batch_size: int, num_samples: int, batch_step, metrics_log) -> Metrics:
         """
         Runs one epoch with the given parameters. Calls the given step function for each batch.
         :param batch_size: the number of samples used at every step
         :param num_samples: the total size of the data set
         :param batch_step: a function that runs and returns the accuracy and loss metrics for the batch
+        :param metrics_log: a function that takes the batch index and all Metrics tuples
         :return: a Metrics tuple of (accuracy, loss), averaged over all batches
         """
         metrics = []
@@ -78,6 +96,7 @@ class ResNetTrainer(BaseTrainer):
         for i in range(num_batches):
             batch_metrics = batch_step()
             metrics.append((batch_metrics.accuracy, batch_metrics.loss))
+            metrics_log(i, metrics)
 
         accuracy_mean, loss_mean = np.mean(metrics, axis=0)
         return Metrics(accuracy_mean, loss_mean)
