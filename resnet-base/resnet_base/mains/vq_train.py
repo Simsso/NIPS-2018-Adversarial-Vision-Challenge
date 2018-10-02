862dd05bda00d9fa99804622c2bc64132f7d2e67
Samed Güner <doktorgibsonyt@gmail.com>
2018-10-02 14:27:07 +0200

    Fix Python stdout
    - redirect tf.logging messages to stdout instead of stderr
    - correctly redirect stderr and stdout to file

--- resnet-base/resnet_base/mains/vq_train.py
+++ resnet-base/resnet_base/mains/vq_train.py
@@ -3,11 +3,20 @@ from resnet_base.model.vq_resnet import VQResNet
 from resnet_base.trainer.resnet_trainer import ResNetTrainer
 from resnet_base.data.tiny_imagenet_pipeline import TinyImageNetPipeline
 from resnet_base.util.logger.factory import LoggerFactory
-
+from logging
+from sys
 
 def main(args):
-    tf.logging.set_verbosity(tf.logging.DEBUG)
+
     tf.set_random_seed(234957)
+
+    tf.logging.set_verbosity(tf.logging.DEBUG)
+    tf_logger = tf_logging._get_logger()
+    handler = logging.StreamHandler(sys.stdout) # create stdout handler
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    tf_logger.handlers = [handler] # redirect tf.logging to stdout instead of stderr
+    tf_logging.propagate = False
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True  # dynamic GPU memory allocation
--- resnet-base/start.sh
+++ resnet-base/start.sh
@@ -6,6 +6,6 @@ python -m resnet_base \
     --pretrained_checkpoint "${HOME}/.data/tiny_imagenet_alp05_2018_06_26.ckpt" \
     --data_dir "${HOME}/.data/tiny-imagenet-200" \
     --train_log_dir "gs://${BUCKET_NAME}/model_data/${MODEL_ID}/train" \
-    --val_log_dir "gs://${BUCKET_NAME}/model_data/${MODEL_ID}/val" > log.txt
+    --val_log_dir "gs://${BUCKET_NAME}/model_data/${MODEL_ID}/val" > log_output.txt 2> &1
 
 sleep infinity
