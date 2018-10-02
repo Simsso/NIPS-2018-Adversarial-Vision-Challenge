94dfad4340f4741d543def38901f56f64738fef9
Timo Denk <github@timodenk.com>
2018-10-02 09:22:40 +0200

    Use replacement

--- resnet-base/resnet_base/mains/vq_train.py
+++ resnet-base/resnet_base/mains/vq_train.py
@@ -14,14 +14,14 @@ def main(args):
     config.gpu_options.allow_growth = True  # dynamic GPU memory allocation
     sess = tf.Session(config=config)
     with sess:
-        pipeline = TinyImageNetPipeline(batch_size=2)
+        pipeline = TinyImageNetPipeline(batch_size=256)
         imgs, labels = pipeline.get_iterator().get_next()
 
         logger_factory = LoggerFactory(TinyImageNetPipeline.num_valid_samples // pipeline.batch_size)
 
         model = VQResNet(logger_factory, imgs, labels)
 
-        trainer = ResNetTrainer(model, pipeline, virtual_batch_size_factor=256)
+        trainer = ResNetTrainer(model, pipeline, virtual_batch_size_factor=1)
         trainer.train()
 
 
--- resnet-base/resnet_base/model/vq_resnet.py
+++ resnet-base/resnet_base/model/vq_resnet.py
@@ -8,7 +8,7 @@ class VQResNet(ResNet):
         x = ResNet._first_conv(x)  # 16x16x64
         with tf.variable_scope(self.custom_scope, auxiliary_name_scope=False):
             x = tf.reshape(x, [-1, 256, 64])
-            vq_endpoints = vq(x, n=1024, alpha=.1, beta=.1, gamma=.1, num_splits=16, lookup_ord=2,
+            vq_endpoints = vq(x, n=128, alpha=.1, beta=.1, gamma=.1, num_splits=16, lookup_ord=2, num_embeds_replaced=1,
                               return_endpoints=True, embedding_initializer=tf.random_normal_initializer(0, stddev=1e-4,
                                                                                                         seed=15092017))
             x = vq_endpoints.layer_out
