7381829aba80058940b88f4205bc6b3291ac604d
Samed Güner <doktorgibsonyt@gmail.com>
2018-10-03 15:25:41 +0200

    Use getLogger() instead of access to protected methods

--- resnet-base/resnet_base/mains/vq_train.py
+++ resnet-base/resnet_base/mains/vq_train.py
@@ -18,7 +18,7 @@ def main(args):
     tf.set_random_seed(15092017)
 
     tf.logging.set_verbosity(tf.logging.DEBUG)
-    tf_logger = tf_logging._get_logger()
+    tf_logger = logging.getLogger('tensorflow')
     handler = logging.StreamHandler(sys.stdout) # create stdout handler
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
