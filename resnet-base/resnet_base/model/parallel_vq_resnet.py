457a1a26218af50c42b2936045c3bb4b0aee7284
Timo Denk <github@timodenk.com>
2018-10-05 17:16:30 +0200

    Move loss logging

--- resnet-base/resnet_base/model/parallel_vq_resnet.py
+++ resnet-base/resnet_base/model/parallel_vq_resnet.py
@@ -24,6 +24,9 @@ class ParallelVQResNet(ResNet):
 
             x = tf.concat(parallel_vq_out, axis=2)
 
+        losses = tf.get_collection(tf.GraphKeys.LOSSES)
+        [log.add_scalar('{}'.format(loss.op.name), loss, log_frequency=10) for loss in losses]
+
         log.add_histogram('vq_out', x, log_frequency=5)
 
         x = tf.reshape(x, [-1, 16, 16, 64])
@@ -34,9 +37,7 @@ class ParallelVQResNet(ResNet):
         log.add_histogram('{}/access_count'.format(i), vq_endpoints.access_count, is_sum_value=True, log_frequency=50)
         log.add_histogram('{}/embedding_space'.format(i), vq_endpoints.emb_space, log_frequency=5)
         log.add_histogram('{}/embedding_spacing'.format(i), vq_endpoints.emb_spacing, log_frequency=5)
-        log.add_scalar('{}/unused_embeddings'.format(i), tf.nn.zero_fraction(vq_endpoints.access_count), log_frequency=10)
-        losses = tf.get_collection(tf.GraphKeys.LOSSES)
-        [log.add_scalar('{}/{}'.format(i, loss.name), loss, log_frequency=10) for loss in losses]
+        log.add_scalar('{}/used_embed'.format(i), 1-tf.nn.zero_fraction(vq_endpoints.access_count), log_frequency=10)
 
     def _build_model(self, x: tf.Tensor) -> tf.Tensor:
         x = ResNet._first_conv(x)  # 16x16x64
