f76a0387c63773f467f6d327a952bcff5ed27904
Samed Güner <doktorgibsonyt@gmail.com>
2018-10-05 18:41:23 +0200

    Add logging and several bash scripts
    - docker_exec.sh for interactive session with the training job
    - watch_smi.sh for watching nvidia-smi

--- resnet-base/start.sh
+++ resnet-base/start.sh
@@ -6,6 +6,6 @@ python -m resnet_base \
     --pretrained_checkpoint "${HOME}/.data/tiny_imagenet_alp05_2018_06_26.ckpt" \
     --data_dir "${HOME}/.data/tiny-imagenet-200" \
     --train_log_dir "gs://${BUCKET_NAME}/model_data/${MODEL_ID}/train" \
-    --val_log_dir "gs://${BUCKET_NAME}/model_data/${MODEL_ID}/val" > log.txt
+    --val_log_dir "gs://${BUCKET_NAME}/model_data/${MODEL_ID}/val" > output/log_output.txt
 
 sleep infinity
