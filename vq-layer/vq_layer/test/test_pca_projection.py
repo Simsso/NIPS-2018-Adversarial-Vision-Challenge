a6de6d718c94e69af831d8db8357ab9ba97a6d20
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2018-10-16 08:42:50 +0200

    Adjust PCA projection tests, add for both batch and emb-space mode

--- vq-layer/vq_layer/test/test_pca_projection.py
+++ vq-layer/vq_layer/test/test_pca_projection.py
@@ -14,42 +14,66 @@ class TestPCAProjection(TFTestCase):
         super(TestPCAProjection, self).setUp()
         np.random.seed(42)
 
-    def feed(self, emb_space_val: Union[List, np.ndarray], x_val: Union[List, np.ndarray], num_components: int) -> List:
+    def feed(self, emb_space_val: Union[List, np.ndarray], x_val: Union[List, np.ndarray], num_components: int,
+             dim_reduction_mode: str = 'pca-batch') -> List:
         x_val = np.array(x_val, dtype=np.float32)
         self.x = tf.placeholder_with_default(x_val, shape=x_val.shape)
         self.x_reshaped = tf.expand_dims(self.x, axis=1)
 
-        endpoints = vq(self.x_reshaped, n=len(emb_space_val), lookup_ord=1,
-                       dim_reduction='pca', num_dim_reduction_components=num_components, return_endpoints=True,
+        endpoints = vq(self.x_reshaped, n=len(emb_space_val), lookup_ord=1, dim_reduction=dim_reduction_mode,
+                       num_dim_reduction_components=num_components, return_endpoints=True,
                        embedding_initializer=tf.constant_initializer(np.array(emb_space_val, dtype=np.float32)))
         self.init_vars()
 
         y = self.sess.run(endpoints.layer_out)
         return y
 
-    def test_projection1(self):
+    def test_batch_projection1(self):
         rand_x = np.random.uniform(low=0, high=10, size=10)
         rand_y = rand_x * 10 + np.random.normal(loc=0, scale=.5, size=10)
         rand_z = rand_x * .1 + np.random.normal(loc=0, scale=.5, size=10)
         x_val = np.stack((rand_x, rand_y, rand_z), axis=1)
 
         emb_space = [[1, -1, 1], [1, 10, .1], [-2, 3, 20], [100, 200, 300]]
-        layer_out = self.feed(emb_space, x_val, num_components=2)
 
         expected = np.array([1, 10, .1] * 10, dtype=np.float32)
         expected = np.reshape(expected, newshape=[10, 1, 3])
 
-        self.assert_numerically_equal(layer_out, expected)
+        layer_out_batch = self.feed(emb_space, x_val, num_components=2, dim_reduction_mode='pca-batch')
+        self.assert_numerically_equal(layer_out_batch, expected)
 
-    def test_projection2(self):
+    def test_emb_space_projection1(self):
+        rand_x = np.random.uniform(low=0, high=10, size=10)
+        rand_y = rand_x * 10 + np.random.normal(loc=0, scale=.5, size=10)
+        rand_z = rand_x * .1 + np.random.normal(loc=0, scale=.5, size=10)
+        x_val = np.stack((rand_x, rand_y, rand_z), axis=1)
+
+        emb_space = [[1, -1, 1], [1, 10, .1], [-2, 3, 20], [100, 200, 300]]
+
+        expected = np.array([1, 10, .1] * 10, dtype=np.float32)
+        expected = np.reshape(expected, newshape=[10, 1, 3])
+
+        layer_out_emb = self.feed(emb_space, x_val, num_components=2, dim_reduction_mode='pca-emb-space')
+        self.assert_numerically_equal(layer_out_emb, expected)
+
+    def test_batch_projection2(self):
         x_val = np.array([[1, -1, 1, -1, 1, -1]])
         emb_space = np.array([[2, -2, 2, -2, 2, -2],
                               [.02, .2, 2, 20, 200, 2000],
                               [10, 20, 40, 80, 160, 320],
                               [1, 1, 1, 1, 1, 1]])
-
         expected = np.array([[2, -2, 2, -2, 2, -2]])
-        layer_out = self.feed(emb_space, x_val, num_components=3)
 
-        self.assert_numerically_equal(layer_out, expected)
+        layer_out_batch = self.feed(emb_space, x_val, num_components=3, dim_reduction_mode='pca-batch')
+        self.assert_numerically_equal(layer_out_batch, expected)
 
+    def test_emb_space_projection2(self):
+        x_val = np.array([[1, -1, 1, -1, 1, -1]])
+        emb_space = np.array([[2, -2, 2, -2, 2, -2],
+                              [.02, .2, 2, 20, 200, 2000],
+                              [10, 20, 40, 80, 160, 320],
+                              [1, 1, 1, 1, 1, 1]])
+        expected = np.array([[2, -2, 2, -2, 2, -2]])
+
+        layer_out_emb = self.feed(emb_space, x_val, num_components=3, dim_reduction_mode='pca-emb-space')
+        self.assert_numerically_equal(layer_out_emb, expected)
