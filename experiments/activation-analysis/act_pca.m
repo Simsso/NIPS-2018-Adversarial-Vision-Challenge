6aafb4e452e843153abf5ec496bfb8c09c56a73e
Timo Denk <github@timodenk.com>
2018-10-18 20:16:14 +0200

    Comment on MATLAB files

--- experiments/activation-analysis/act_pca.m
+++ experiments/activation-analysis/act_pca.m
@@ -1,12 +1,23 @@
-act = act5_block3;
+%{
+Takes a tensor of shape [n,m,o,p], which is a common shape for CNN
+activations, where n is the batch, m and o are spatial, and p are the
+channels. The tensor is flattened, such that the actiations of the layer
+are a single vector of size m*o*p. A PCA is then being computed on the 
+resulting matrix. The first dim_out columns of the PCA output matrix, which
+can be used e.g. for compression, is then written to a file at export_path.
+%}
+
+act = act5_block3; % CNN activation tensor (n, m, o, p)
+export_path = '/Users/timodenk/.data/activations/pca.mat' % PCA output file
+dim_out = 3968 % number of PCA output vectors to use, must be <= m*o*p
+
 act_shape = size(act)
 
 num_samples = act_shape(1)
-channels = reshape(act, num_samples, act_shape(2)*act_shape(3)*act_shape(4));
+vec_size = act_shape(2)*act_shape(3)*act_shape(4)
+channels = reshape(act, num_samples, vec_size); % flatten the activations
 
-pca_out = pca(channels);
-dim_out = 3968
-pca_out = pca_out(:,1:dim_out);
+pca_out = pca(channels);  % compute PCA
+pca_out = pca_out(:,1:dim_out); % grab top dim_out eigenvectors
 
-export_path = '/Users/timodenk/.data/activations/pca.mat'
 save(export_path, 'pca_out')
--- experiments/activation-analysis/export_tests.m
+++ experiments/activation-analysis/export_tests.m
@@ -1,3 +1,8 @@
+%{
+Writes a random matrix to the file at export_path. Created for testing
+purposes, namely importing to Python.
+%}
+
 export_path = '/Users/timodenk/.data/activations/test_export.mat'
 mat = rand(64, 61);
 save(export_path, 'mat')
