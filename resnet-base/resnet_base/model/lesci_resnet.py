34953a64501c379d02af53bef60aaaf87b3a649d
Timo Denk <github@timodenk.com>
2018-10-20 12:33:40 +0200

    Disable PCA-based lookup

--- resnet-base/resnet_base/model/lesci_resnet.py
+++ resnet-base/resnet_base/model/lesci_resnet.py
@@ -17,9 +17,9 @@ class LESCIResNet(ResNet):
     def _lesci_layer(self, x: tf.Tensor, shape: List[int], mat_name: str) -> tf.Tensor:
         assert len(shape) == 2
         with tf.variable_scope(self.custom_scope, auxiliary_name_scope=False):
-            return cvq(x, shape[0], 'pca-emb-space', num_dim_reduction_components=2048,
-                   embedding_initializer=self._make_init(FLAGS.lesci_emb_space_file, shape=shape, mat_name=mat_name),
-                   constant_init=True, num_splits=1, return_endpoints=False)
+            return cvq(x, shape[0], constant_init=True, num_splits=1, return_endpoints=False,
+                       embedding_initializer=self._make_init(FLAGS.lesci_emb_space_file, shape=shape,
+                                                             mat_name=mat_name))
 
     def _make_init(self, mat_file_path: str, shape: List[int], mat_name: str = 'emb_space'):
         """
@@ -48,8 +48,8 @@ class LESCIResNet(ResNet):
         x = ResNet._v2_block(x, 'block1', base_depth=64, num_units=3, stride=2)
         x = ResNet._v2_block(x, 'block2', base_depth=128, num_units=4, stride=2)
         x = ResNet._v2_block(x, 'block3', base_depth=256, num_units=6, stride=2)  # 2x2x1024
-        x = tf.reshape(x, [-1, 1, 2*2*1024])
-        x = self._lesci_layer(x, shape=[32768, 2*2*1024], mat_name='act5_block3_small')
+        x = tf.reshape(x, [-1, 1, 2 * 2 * 1024])
+        x = self._lesci_layer(x, shape=[32768, 2 * 2 * 1024], mat_name='act5_block3_small')
         x = tf.reshape(x, [-1, 2, 2, 1024])
         x = ResNet._v2_block(x, 'block4', base_depth=512, num_units=3, stride=1)
         x = ResNet.batch_norm(x)
