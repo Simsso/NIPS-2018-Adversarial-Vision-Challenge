5d1a40f9c37f86b107ed592524a12335c5a1f73e
Timo Denk <github@timodenk.com>
2018-10-20 12:39:24 +0200

    Experiment with cosine similarity histograms

---
+++ experiments/activation-analysis/act_cosine_similarity.m
@@ -0,0 +1,24 @@
+act = act5_block3;
+labels = transpose(target_labels);
+sample_index = 4
+
+act_shape = size(act)
+vec_size = act_shape(2)*act_shape(3)*act_shape(4)
+num_samples = act_shape(1)
+
+act = reshape(act, num_samples, vec_size);
+
+% normalize vectors
+act = normr(act);
+
+sample_vec = transpose(act(sample_index:sample_index,:))
+sample_label = labels(sample_index)
+cos_sim = act*sample_vec;
+h1 = histogram(cos_sim);
+h1.BinWidth = 0.01
+hold on
+
+same_class_act = act(labels==sample_label,:);
+cos_sim_same_class = same_class_act*sample_vec;
+h2 = histogram(cos_sim_same_class);
+h2.BinWidth = 0.01
