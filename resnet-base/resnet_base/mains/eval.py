5bb1f909f779aa1d01e8ccf1fb01301e9cadad33
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2018-10-25 08:17:49 +0200

    Add LESCI-accuracy metrics to eval script

--- resnet-base/resnet_base/mains/eval.py
+++ resnet-base/resnet_base/mains/eval.py
@@ -1,11 +1,9 @@
-from typing import Dict
-
+from typing import Tuple
+import numpy as np
+import tensorflow as tf
 from tensorflow.python.framework.errors_impl import InvalidArgumentError
 
 from resnet_base.data.tiny_imagenet_pipeline import TinyImageNetPipeline
-import numpy as np
-import tensorflow as tf
-
 from resnet_base.model.lesci_resnet import LESCIResNet
 from resnet_base.model.resnet import ResNet
 
@@ -13,7 +11,8 @@ BATCH_SIZE = 100  # adjustment based on available RAM
 tf.logging.set_verbosity(tf.logging.DEBUG)
 
 
-def run_validation(model: ResNet, pipeline: TinyImageNetPipeline, mode: tf.estimator.ModeKeys) -> Dict[float, float]:
+def run_validation(model: LESCIResNet, pipeline: TinyImageNetPipeline, mode: tf.estimator.ModeKeys)\
+        -> Tuple[float, float]:
     """
     Feeds all validation/train samples through the model and report classification accuracy and loss.
     :return: Dictionary of mean accuracy and mean loss
@@ -36,18 +35,26 @@ def run_validation(model: ResNet, pipeline: TinyImageNetPipeline, mode: tf.estim
 
         tf.logging.info("Starting evaluation")
         vals = []
-        acc_mean_val, loss_mean_val = 0., 0.
+        acc_mean_val, loss_mean_val, acc_proj_mean_val, acc_id_mean_val, id_mapped_mean_val = 0., 0., 0., 0., 0.
         num_samples = pipeline.get_num_samples(mode)
         n = num_samples // BATCH_SIZE
         missed_samples = num_samples % BATCH_SIZE
         if missed_samples > 0:
             tf.logging.warning("Omitting {} samples because the batch size ({}) is not a divisor of the number of "
                                "samples ({}).".format(missed_samples, num_samples, BATCH_SIZE))
+
+        fetches = [model.accuracy, model.loss, model.accuracy_projection, model.accuracy_identity,
+                   model.percentage_identity_mapped]
         for i in range(n):
-            vals.append(sess.run([model.accuracy, model.loss]))
-            acc_mean_val, loss_mean_val = np.mean(vals, axis=0)
-            tf.logging.info("[{}/{}] Current accuracy: {}".format(i, n, acc_mean_val))
-        tf.logging.info("[Done] Mean: accuracy {}, loss {}".format(acc_mean_val, loss_mean_val))
+            vals.append(sess.run(fetches))
+            acc_mean_val, loss_mean_val, acc_proj_mean_val, acc_id_mean_val, id_mapped_mean_val = np.mean(vals, axis=0)
+            tf.logging.info("[{:,}/{:,}] Current overall accuracy: {:.3f}\tprojection: {:.3f}\tid-mapping: {:.3f}"
+                            "\tpercentage id-mapped: {:.3f}"
+                            .format(i, n, acc_mean_val, acc_proj_mean_val, acc_id_mean_val, id_mapped_mean_val))
+
+        tf.logging.info("[Done] Mean: accuracy {:.3f}, projection accuracy {:.3f}, identity mapping accuracy {:.3f}, "
+                        "loss {:.3f}, id-mapped {:.3f}"
+                        .format(acc_mean_val, acc_proj_mean_val, acc_id_mean_val, loss_mean_val, id_mapped_mean_val))
     return acc_mean_val, loss_mean_val
 
 
