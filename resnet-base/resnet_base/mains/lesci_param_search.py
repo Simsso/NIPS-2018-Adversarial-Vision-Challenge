f5e0fff0efc8ea635fd531c651505a62a1cccb6b
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2018-10-30 07:51:20 +0100

    Setup flags in LESCIExperiment, log results

--- resnet-base/resnet_base/mains/lesci_param_search.py
+++ resnet-base/resnet_base/mains/lesci_param_search.py
@@ -9,7 +9,7 @@ BATCH_SIZE = 100
 CODE_SIZES = {
     'act6_block4': [128, 256, 512, 1024],
     'act8_global_avg': [32, 128, 512],
-    'act5_block3': [512, 1024]
+    # 'act5_block3': [512, 1024]
 }
 
 KS = [1, 10, 20, 100]
@@ -18,16 +18,30 @@ ACCURACY_CONSTRAINTS = [0.7, 0.75, 0.8]
 
 
 def criterion(metrics: LESCIMetrics) -> float:
-    return 1 - metrics.percentage_identity_mappe
+    return 1 - metrics.percentage_identity_mapped
 
 
 def grid_search():
+    best_experiment = None
+    best_score = -1
+
     for lesci_pos in CODE_SIZES.keys():
         for code_size in CODE_SIZES[lesci_pos]:
             for proj_thres in PROJECTION_THRESHOLDS:
                 for min_accurary in ACCURACY_CONSTRAINTS:
-                    for k in KS:
-                        experiment = LESCIExperiment(lesci_pos, code_size, proj_thres, k, min_accurary)
-                        experiment.run()
+                    for k in KS:                                                # TODO emb_size??
+                        experiment = LESCIExperiment(lesci_pos, code_size, proj_thres, k, 0, min_accurary)
 
-                        # TODO manage results
+                        try:
+                            experiment.run()
+                            experiment.print_results()
+
+                            score = criterion(experiment.metrics)
+                            if best_experiment is None or score > best_score:
+                                best_experiment = experiment
+                                best_score = score
+                                tf.logging.info("*** This is the new best experiment! Score: {}. ***".format(score))
+
+                        except:
+                            tf.logging.error("Error occurred while executing experiment: {}"
+                                             .format(experiment.experiment_description()))
--- resnet-base/resnet_base/util/lesci_experiment.py
+++ resnet-base/resnet_base/util/lesci_experiment.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
+import os
 
 from data.tiny_imagenet_pipeline import TinyImageNetPipeline
 from model.baseline_lesci_resnet import BaselineLESCIResNet
-from util.validation import run_validation
+from util.validation import run_validation, LESCIMetrics
 
 BATCH_SIZE = 100
 FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_string("mat_base_dir", os.path.expanduser('~/.models/activations/baseline/'),
+                       "The directory in which all needed .mat files can be found.")
 
 
 class LESCIExperiment:
@@ -13,26 +17,55 @@ class LESCIExperiment:
     Represents one hyper-parameter-tuning experiment for the BaselineLESCIResNet.
     """
 
-    def __init__(self, lesci_pos: str, code_size: int, proj_thres: float, k: int, min_accurary: float):
+    def __init__(self, lesci_pos: str, code_size: int, proj_thres: float, k: int, emb_size: int, min_accurary: float):
         """
         :param lesci_pos: the position of the LESCI-layer (e.g. 'act6-block5')
         :param code_size: the PCA-compression size for the VQ-layer
         :param proj_thres: the threshold of label-consensus needed to project a sample (in the VQ-layer)
         :param k: the number of neighbors considered by the VQ-layer
+        :param emb_size: the number of embedding vectors
         :param min_accurary: the minimum mean accuracy on the validation set that this experiments needs to reach
         """
         self.lesci_pos = lesci_pos
         self.code_size = code_size
         self.proj_thres = proj_thres
         self.k = k
+        self.emb_size = emb_size
         self.min_accuracy = min_accurary
 
-        self.metrics = None
+        self.metrics: LESCIMetrics = None
 
     def run(self):
+        # set up the model's flags so it uses the correct matrices corresponding to this experiment
+        FLAGS.pca_compression_file = FLAGS.mat_base_dir + 'pca_{}_{}.mat'.format(self.lesci_pos, self.code_size)
+        FLAGS.lesci_emb_space_file = FLAGS.mat_base_dir + 'lesci_{}_{}.mat'.format(self.lesci_pos, self.code_size)
+
+        # set up the pipeline, create the model
         pipeline = TinyImageNetPipeline(physical_batch_size=BATCH_SIZE, shuffle=False)
         imgs, labels = pipeline.get_iterator().get_next()
 
         model = BaselineLESCIResNet(lesci_pos=self.lesci_pos, code_size=self.code_size, proj_thres=self.proj_thres,
-                                    k=self.k, x=imgs, labels=labels)
+                                    k=self.k, emb_size=self.emb_size, x=imgs, labels=labels)
+
         self.metrics = run_validation(model, pipeline, mode=tf.estimator.ModeKeys.EVAL)
+
+    def experiment_description(self):
+        return "-- Parameters: \n" + \
+               "  - lesci_pos: {}\n" + \
+               "  - code_size: {}\n" + \
+               "  - proj_thres: {}\n" + \
+               "  - k: {}\n" + \
+               "  - min_accuracy: {}".format(self.lesci_pos, self.code_size, self.proj_thres, self.k, self.min_accuracy)
+
+    def print_results(self):
+        min_accuracy_reached = "YES" if self.metrics.accuracy >= self.min_accuracy else "NO"
+        tf.logging.info("----- Experiment completed -----\n"
+                        "{}\n"
+                        "-- Results: \n"
+                        "  - accuracy: {}\n"
+                        "  - min_accuracy reached: {}\n"
+                        "  - percentage_id_mapped: {}\n"
+                        "  - accuracy_projection: {}\n".format(self.experiment_description(), self.metrics.accuracy,
+                                                               min_accuracy_reached,
+                                                               self.metrics.percentage_identity_mapped,
+                                                               self.metrics.accuracy_projection))
